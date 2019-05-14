27ee924c44523fe8d34a23504dfaf980c5e5ee1d
Usuario <Usuario@DESKTOP-7IM02CJ>
2019-05-14 19:17:02 -0300

    .Net

---
+++ Hola mundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hola !!");
+            Console.ReadKey();
+        }
+    }
+}
